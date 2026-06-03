cb113d74d6990572b1d52a29d7299dc200b46fa1
Bhavishya <bhavishya0045@gmail.com>
2026-06-03 22:01:57 +0530

    Add files via upload

---
+++ Day - 1/C Lang/Sum of N Natural Numbers.c
@@ -0,0 +1,15 @@
+
+# A Program to Calculate Sum of first N natural numbers.
+
+#include <stdio.h>
+
+int main() {
+    int num, sum;
+
+    printf("Enter a number: ");
+    scanf("%d", &N);
+
+    sum = num * (num + 1) / 2;
+
+    printf("Sum of First %d natural numbers is: %d\n", num, sum);
+}
