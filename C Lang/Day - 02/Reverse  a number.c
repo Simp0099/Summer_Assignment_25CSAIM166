722842b2e353ba876cd6939a716f0fb9fd1cddb8
Bhavishya <bhavishya0045@gmail.com>
2026-06-04 08:44:17 +0530

    Modified

--- C Lang/Day - 02/Reverse  a number.c
+++ C Lang/Day - 02/Reverse  a number.c
@@ -1,14 +1,19 @@
-//to print reverse of number
+
+// Program to print the reverse of a number.
+
 #include <stdio.h>
 int main() {
-    int num, remainder , sum, reverse = 0;
-    printf("Enter the series : ");
+    int num, remainder, sum = 0, reverse = 0;
+
+    printf("Enter a number: ");
     scanf("%d", &num);
+
     while (num != 0) {
         remainder=num%10;
         sum = sum+remainder;
         reverse = reverse * 10 + remainder;
         num/=10;}
+        
     printf("The reverse of the number : %d", reverse);
     printf("\nThe sum of the digits : %d", sum);
 }
